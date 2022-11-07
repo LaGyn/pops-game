dc06818838f0a91c6f42c4c38940de809588cd98
Laura Gynther <lauragynther@live.com>
2022-11-07 09:03:07 +0200

    lisättiin komponentteja: pallo, pommi

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById('gameArea');
-let ctx = canvas.getContext('2d');
+//let ctx = canvas.getContext('2d');
 
 function resize(){
     canvas.width = window.innerWidth;
@@ -11,37 +11,35 @@ window.addEventListener('resize', resize);
 //document.addEventListener('keydown', liiku);
 //document.addEventListener('keydown', vaihdasuunta);
 
-let pallo;
-
 let button = document.getElementById('load');
 button.addEventListener('click', startGame);
-/*
+
 let pelialue = {
     canvas: document.getElementById('gameArea'),
-    startGame: function() {
+    start: function() {
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
         this.ctx = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     }
-}*/
+}
 
+let pallo;
+let pommi;
+let colors = ['pink', 'green', 'yellow', 'orange'];
 function startGame(){
     let frontPage = document.getElementById('tausta');
     frontPage.style.display = 'none';
-    /*for (let i = 0; i < frontPage.length; i++){
-        frontPage[i].style.display = 'none';
-    }*/
+    pelialue.start();
     canvas.style.display = "block";
-    /*
-    pallo = new Component(45, window.innerHeight-70, "red") 
-    pallo.style.left = "0px";
-    let pommi = new Component(600, 40, "pink")*/
+    pallo = new component(45, window.innerHeight-70, "red") 
+    pommi = new component(600, 40, "pink")
 }
-/*
-function Component(x, y, color){
+
+function component(x, y, color){
     this.x = x;
     this.y = y;
-    //ctx = pelialue.ctx;
+    ctx = pelialue.ctx;
     ctx.beginPath();
     ctx.arc(this.x, this.y, 40, 0, 2 * Math.PI);
     ctx.strokeStyle = color;
@@ -50,7 +48,7 @@ function Component(x, y, color){
     ctx.fill();
 }
 
-
+/*
 let nopeusX = 5;
 
 function liiku(event){//tehdään pallon liikkuminen mahdollisesksi:
